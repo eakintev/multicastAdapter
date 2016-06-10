0479384715f3889ff1af93a88d03250851a77864
Evgeny Akintev <eakintev@gmail.com>
2016-06-11 00:59:49 +0300

    "added basic project files"

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+// network libraries
+#include <winsock2.h>
+
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
